777fb83b0db19746843ed925fea05da300a46e31
Kaizhi Wei <wei.kaizhi@gmail.com>
2012-07-26 21:57:06 -0400

    adding version and OS check

--- SublimeUpdater.py
+++ SublimeUpdater.py
@@ -4,31 +4,53 @@ import shutil
 import urlparse
 import os
 import threading 
+import json
 
+DOWNLOAD_URL = "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64%20Setup.exe"
+UPDATE_URL = "http://www.sublimetext.com/updates/2/stable/updatecheck" #url to check for latest st2 version
 
-def download(url, fileType="None"):
-	file_name = url.split('/')[-1]
-	u = urllib2.urlopen(url)
-	f = open(file_name, 'wb')
-	meta = u.info()
-	file_size = int(meta.getheaders("Content-Length")[0])
-	print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-	file_size_dl = 0
-	block_sz = 8192
-	while True:
-	    buffer = u.read(block_sz)
-	    if not buffer:
-	        break
-
-	    file_size_dl += len(buffer)
-	    f.write(buffer)
-
-	f.close()
 
 
 class ExampleCommand(sublime_plugin.ApplicationCommand):  
+
+	def download(url, fileType="None"):
+		file_name = url.split('/')[-1]
+		u = urllib2.urlopen(url)
+		f = open(file_name, 'wb')
+		meta = u.info()
+		file_size = int(meta.getheaders("Content-Length")[0])
+		print "Downloading: %s Bytes: %s" % (file_name, file_size)
+
+		file_size_dl = 0
+		block_sz = 8192
+		while True:
+		    buffer = u.read(block_sz)
+		    if not buffer:
+		        break
+
+		    file_size_dl += len(buffer)
+		    f.write(buffer)
+
+		f.close()
+
+	def getLatestVersion():
+		data = urllib2.urlopen(UPDATE_URL).read()
+		data = json.loads(data)
+		return data['latest_version']
+
 	def run(self):
-		thr = threading.Thread(target=download, args=('http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.1%20x64%20Setup.exe',))
-		thr.start() # will run "foo"
+		if sublime.platform() == "windows":
+			if getLatestVersion() == int(sublime.version()):
+				print ("currently on latest version")
+			else:
+				print ("new version available")
+				#download the latest installer
+				thr = threading.Thread(target=download, args=(DOWNLOAD_URL,))
+				thr.start()
+	
+		elif sublime.platform() == "linux":
+			print "linux detected"
+	
+		elif sublime.platform() == "osx":
+			print "mac detected"
 
